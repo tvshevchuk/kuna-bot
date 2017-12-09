157c63c101dbb6b4eaa325279dd6766c47b6066f
tshevchuk <tvshevchuk@gmail.com>
2017-12-09 17:30:26 +0200

    Added ability to fetch last KUNA market data

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const hbs = require('hbs');
+const request = require('request');
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -7,10 +8,35 @@ const port = process.env.PORT || 8080;
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname));
 
+const lastMarketData = [];
+
+setInterval(() => {
+    request({
+        url: 'https://kuna.io/api/v2/tickers/btcuah',
+        json: true
+    }, (err, response, body) => {
+        lastMarketData.push(body);
+        if (lastMarketData.length >= 10) {
+            lastMarketData.shift();
+        }
+    });
+}, 5000);
+
+const renderLastMarketData = (lastMarketData) => {
+    let temp = '<ul>';
+    lastMarketData.forEach(el => {
+        temp += '<li>' + JSON.stringify(el) + '</li>';
+    });
+    return temp + '</ul>';
+}
+
 app.get('/', (req, res) => {
-    res.render('index.hbs');
+    let lastMarketDataHtml = renderLastMarketData(lastMarketData);
+    res.render('index.hbs', {
+        lastMarketDataHtml
+    });
 });
 
 app.listen(port, () => {
     console.log(`Listening on ${port} port...`);
-})
+});
