e5dadf23fc5ae0224284d8805e1a501223a0c8a5
tshevchuk <tvshevchuk@gmail.com>
2017-12-09 15:56:10 +0200

    Fixed env port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 
 const app = express();
-const port = process.env.NODE_ENV || 8080;
+const port = process.env.PORT || 8080;
 
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname));
