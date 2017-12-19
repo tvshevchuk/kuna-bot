d414ebcb1c8bfc3ecd80f4c793565a4126bc0dda
Taras Shevchuk <tshevchuk@blackberry.com>
2017-12-19 18:17:11 +0200

    Added newrelic

---
+++ newrelic.js
@@ -0,0 +1,25 @@
+'use strict'
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['kuna-bot'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: 'f5394f11f44ef0b6ece60da806b517542e9bbe4f',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
